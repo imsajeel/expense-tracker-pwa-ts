fab1c338373f27a2dac7fcdd1dc864cb16ddc6c1
Sajeel Aalam <imsajeel.dev@gmail.com>
2021-01-22 15:53:23 +0500

    transformed to typescript

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,7 +4,12 @@ self.importScripts(
 );
 
 firebase.initializeApp({
+  apiKey: "AIzaSyBb-Pi26sGnGMrs1cFSfebSn30fvPBIXLA",
+  authDomain: "expense-tracker-f97ef.firebaseapp.com",
+  projectId: "expense-tracker-f97ef",
+  storageBucket: "expense-tracker-f97ef.appspot.com",
   messagingSenderId: "564575562330",
+  appId: "1:564575562330:web:ae230500488d744a284663",
 });
 
 firebase.messaging();
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Balance from "./components/Balance";
@@ -12,17 +12,20 @@ import firebase from "./firebase";
 function App() {
   const messaging = firebase.messaging();
 
-  messaging
-    .requestPermission()
-    .then(() => {
-      return messaging.getToken();
-    })
-    .then((token) => {
-      alert(token);
-      prompt("token", token);
-      console.log("token", token);
-    })
-    .catch((err) => console.log("error", err));
+  useEffect(() => {
+    console.log("use effect is runnning...");
+    messaging
+      .requestPermission()
+      .then(() => {
+        return messaging.getToken();
+      })
+      .then((token) => {
+        alert(token);
+        prompt("token", token);
+        console.log("token", token);
+      })
+      .catch((err) => console.log("error", err));
+  }, [messaging]);
 
   return (
     <GlobalProvider>
